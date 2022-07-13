c5e0419a57ea9089f8a4a5b498862f29ed1337ee
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-07-13 18:03:57 +0530

    Add: Email and Password container views

--- FireChatMVVM/Controller/Authentication/LoginController.swift
+++ FireChatMVVM/Controller/Authentication/LoginController.swift
@@ -17,6 +17,78 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let containerview = UIView()
+        
+        let iv = UIImageView()
+        let image = UIImage(systemName: "envelope")
+        iv.image = image
+        iv.tintColor = .white
+        
+        containerview.addSubview(iv)
+        iv.centerY(inView: containerview)
+        iv.anchor(left: containerview.leftAnchor, paddingLeft: 8)
+        iv.setDimensions(height: 24, width: 24)
+        
+        containerview.addSubview(emailTextField)
+        emailTextField.centerY(inView: containerview)
+        emailTextField.anchor(left: iv.rightAnchor,
+                              bottom: containerview.bottomAnchor,
+                              right: containerview.rightAnchor,
+                              paddingLeft: 8,
+                              paddingBottom: 4)
+        
+        containerview.setHeight(height: 50)
+        return containerview
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let containerview = UIView()
+        
+        let iv = UIImageView()
+        let image = UIImage(systemName: "lock")
+        iv.image = image
+        iv.tintColor = .white
+        
+        containerview.addSubview(iv)
+        iv.centerY(inView: containerview)
+        iv.anchor(left: containerview.leftAnchor, paddingLeft: 8)
+        iv.setDimensions(height: 24, width: 24)
+        
+        containerview.addSubview(passwordTextField)
+        passwordTextField.centerY(inView: containerview)
+        passwordTextField.anchor(left: iv.rightAnchor,
+                                 bottom: containerview.bottomAnchor,
+                                 right: containerview.rightAnchor,
+                                 paddingLeft: 8,
+                                 paddingBottom: 4)
+        
+        containerview.setHeight(height: 50)
+        return containerview
+    }()
+    
+    private let authButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.backgroundColor = .systemRed
+        button.setHeight(height: 50)
+        return button
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter email"
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter password"
+        textField.isSecureTextEntry = true
+        return textField
+    }()
     
     // MARK: Lifecycle
     override func viewDidLoad() {
@@ -34,11 +106,24 @@ class LoginController: UIViewController {
         configureGradientLayer()
         
         view.addSubview(iconImage)
-        iconImage.translatesAutoresizingMaskIntoConstraints = false
-        iconImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        iconImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        iconImage.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        iconImage.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        iconImage.setDimensions(height: 120, width: 120)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailContainerView,
+                                                       passwordContainerView,
+                                                       authButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        
+        view.addSubview(stackView)
+        
+        stackView.anchor(top: iconImage.bottomAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: 32,
+                         paddingLeft: 32,
+                         paddingRight: 32)
     }
     
     func configureGradientLayer(){
