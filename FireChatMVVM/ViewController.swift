29ffbd3c296b9af0a1ed1d201874ce5d4ac39f33
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-07-13 15:44:33 +0530

    Initial Commit

---
+++ FireChatMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChatMVVM
+//
+//  Created by Avijeet Pandey on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
