29713059b5918dde58a10bbd8c6eecf421d127e7
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-07-13 16:09:38 +0530

    Add : Navbar

---
+++ FireChatMVVM/Controller/ConversationsController.swift
@@ -0,0 +1,49 @@
+//
+//  ConversationsController.swift
+//  FireChatMVVM
+//
+//  Created by Avijeet Pandey on 13/07/22.
+//
+
+import Foundation
+import UIKit
+
+class ConversationsController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    @objc func showProfile(){
+        print(123)
+    }
+    
+    // MARK: - Helpers
+    func configureUI(){
+        view.backgroundColor = .white
+        configureNavBar()
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    func configureNavBar(){
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+    }
+}
