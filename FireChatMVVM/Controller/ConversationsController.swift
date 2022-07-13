db64930d2514cd0f94e875e53fd335386b13a023
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-07-13 16:24:26 +0530

    Add: Tableview extensions

--- FireChatMVVM/Controller/ConversationsController.swift
+++ FireChatMVVM/Controller/ConversationsController.swift
@@ -8,13 +8,17 @@
 import Foundation
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
     // MARK: - Properties
+    private let tableView = UITableView()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureTableView()
     }
     
     // MARK: - Selectors
@@ -25,9 +29,23 @@ class ConversationsController: UIViewController {
     // MARK: - Helpers
     func configureUI(){
         view.backgroundColor = .white
+        
         configureNavBar()
+        
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    func configureTableView(){
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
     }
     
     func configureNavBar(){
@@ -46,4 +64,23 @@ class ConversationsController: UIViewController {
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.isTranslucent = true
     }
+}
+
+// MARK: Extensions
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "My text"
+        return cell
+    }
+    
+    
+}
+
+extension ConversationsController: UITableViewDelegate {
+    
 }
